f4285a1f8886604dab7ee8481a3f109aca612103
Mohammedattalla1996 <attallamohammed46@gmail.com>
2023-06-22 19:38:06 +0300

    TODO 9 handle Exception for selection input

--- final project.py
+++ final project.py
@@ -70,50 +70,51 @@ class Student:
 # in Global Scope
 # TODO 8 declare empty students list
 students = []
-
 while True:
 
     # TODO 9 handle Exception for selection input
-    selection = int(input("1.Add New Student\n"
-                          "2.Delete Student\n"
-                          "3.Display Student\n"
-                          "4.Get Student Average\n"
-                          "5.Add Course to student with mark.\n"
-                          "6.Exit"))
+    try:
+        selection = int(input("1.Add New Student\n"
+                              "2.Delete Student\n"
+                              "3.Display Student\n"
+                              "4.Get Student Average\n"
+                              "5.Add Course to student with mark.\n"
+                              "6.Exit"))
 
-    if selection == 1:
+        if selection == 1:
 
-        # TODO 10 make sure that Student number is not exists before
-        student_number = input("Enter Student Number")
+            # TODO 10 make sure that Student number is not exists before
+            student_number = input("Enter Student Number")
 
-        student_name = input("Enter Student Name")
-        while True:
-            try:
-                student_age = int(input("Enter Student Age"))
-                break
-            except:
-                print("Invalid Value")
+            student_name = input("Enter Student Name")
+            while True:
+                try:
+                    student_age = int(input("Enter Student Age"))
+                    break
+                except:
+                    print("Invalid Value")
 
-        # TODO 11 create student object and append it to students list
+            # TODO 11 create student object and append it to students list
 
-        print("Student Added Successfully")
+            print("Student Added Successfully")
 
-    elif selection == 2:
-        student_number = input("Enter Student Number")
-        # TODO 12 find the target student using loops and delete it if exist , if not print ("Student Not Exist")
+        elif selection == 2:
+            student_number = input("Enter Student Number")
+            # TODO 12 find the target student using loops and delete it if exist , if not print ("Student Not Exist")
 
-    elif selection == 3:
-        student_number = input("Enter Student Number")
-        # TODO 13 find the target student using loops and print student detials  if exist , if not print ("Student Not Exist")
+        elif selection == 3:
+            student_number = input("Enter Student Number")
+            # TODO 13 find the target student using loops and print student detials  if exist , if not print ("Student Not Exist")
 
-    elif selection == 4:
-        student_number = input("Enter Student Number")
-        # TODO 14 find the target student using loops and get student average  if exist , if not print ("Student Not Exist")
+        elif selection == 4:
+            student_number = input("Enter Student Number")
+            # TODO 14 find the target student using loops and get student average  if exist , if not print ("Student Not Exist")
 
-    elif selection == 5:
-        student_number = input("Enter Student Number")
-        # TODO 15 ask user to enter course name and course mark then create coures object then append it to target student courses
-
-    else:
+        elif selection == 5:
+            student_number = input("Enter Student Number")
+            # TODO 15 ask user to enter course name and course mark then create coures object then append it to target student courses
         # TODO 16 call a function to exit the program
-        pass
+        else:
+            pass
+    except ValueError:
+        print("Invalid Selection")
